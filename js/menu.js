859eb4a8b1f8468c12ba06dbd56a8318573e1509
szaroket <mpiestrzynska@interia.pl>
2018-04-12 13:04:26 +0200

    Add basic version of contact form

--- js/menu.js
+++ js/menu.js
@@ -2,8 +2,8 @@ var status = false;
 
 function openOrCloseNav() {
   if (document.getElementById("menu-button").checked === true){
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidenav").style.width = "230px";
+    document.getElementById("main").style.marginLeft = "230px";
   } else {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
